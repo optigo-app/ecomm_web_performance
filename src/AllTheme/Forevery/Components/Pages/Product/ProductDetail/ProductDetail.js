c2fb3298b971c773d162ae8ba1a198c1f911fc5b
rajan <rajan@gmail.com>
2025-01-10 18:03:51 +0530

    ddddd

--- src/AllTheme/Forevery/Components/Pages/Product/ProductDetail/ProductDetail.js
+++ src/AllTheme/Forevery/Components/Pages/Product/ProductDetail/ProductDetail.js
@@ -1480,6 +1480,54 @@ const ProductDetail = () => {
     }
 
   }
+  const imageRefs = useRef([])
+  const [origin, setorigin] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e, index) => {
+    const imageRef = imageRefs?.current[index];
+    if (!imageRef) return;
+
+    const imageContainer = imageRef?.parentElement;
+    if (!imageContainer) return;
+
+    const rect = imageContainer.getBoundingClientRect();
+    const x = (e.clientX - rect.left) / rect.width;
+    const y = (e.clientY - rect.top) / rect.height;
+
+    const zoomLevel = 1.5;
+
+    const transformOriginX = x * 100;
+    const transformOriginY = y * 100;
+    console.log(transformOriginX ,
+      transformOriginY , "xy")
+
+    setorigin({ x: transformOriginX, y: transformOriginY })
+    imageRef.style.transformOrigin = `${transformOriginX}% ${transformOriginY}%`;
+    imageRef.style.transform = `scale(${zoomLevel})`;
+  };
+
+  const handleMouseLeave = (index) => {
+    const imageReff = imageRefs?.current[index]
+    if (!imageReff) return
+
+    imageReff.style.transform = 'scale(1)';
+    imageReff.style.transformOrigin = 'center center'; // reset the origin to center
+    setorigin({ x: 0, y: 0 })
+
+  }
+
+  useEffect(() => {
+    // Cleanup logic when component unmounts or ref changes
+    return () => {
+      // Reset all image transformations
+      imageRefs.current.forEach((ref) => {
+        if (ref) {
+          ref.style.transform = 'scale(1)';
+          ref.style.transformOrigin = 'center center';
+        }
+      });
+    };
+  }, []);
 
   return (
     <div className="for_ProductDet_mainDiv">
@@ -1564,14 +1612,28 @@ const ProductDetail = () => {
                                 <Slider {...settings} ref={sliderRef} lazyLoad="progressive">
                                   {PdImageArr?.length > 0 && PdImageArr.map((val, i) => (
                                     <div key={i} className="for_slider_card">
-                                      <div className="for_image">
+                                      <div className="for_image"
+                                        style={{
+                                          position: "relative",
+                                          overflow: "hidden",
+                                          cursor: "zoom-in",
+                                          width: "100%",
+                                          transition:"0.3s ease-in"
+                                        }}
+                                        onMouseMove={(e) => handleMouseMove(e, i)}
+                                        onMouseLeave={() => handleMouseLeave(i)}
+                                      >
                                         {val?.type === "img" ? (
                                           <img
+                                          ref={el => imageRefs.current[i] = el}
                                             loading="lazy"
                                             src={val?.src}
                                             alt=""
-                                            style={{ transition: 'transform 0.3s ease' }}
                                             onLoad={() => setIsImageLoad(false)}
+                                            onError={(e) => {
+                                              e.target.onerror = null;
+                                              e.target.src = imageNotFound;
+                                            }}
                                           />
                                         ) : (
                                           <div style={{ height: "80%" }}>
