1f6cc7c15b928bfbed5a515e6ce8e2a1e77b3a7e
rajan <rajan@gmail.com>
2025-01-10 18:04:33 +0530

    t

--- src/ThemeRoutes.js
+++ src/ThemeRoutes.js
@@ -1,7 +1,7 @@
 import React, { Suspense, useEffect, useRef, useState } from "react";
 import SmilingRock_App from "./AllTheme/SmilingRock/SmilingRock_App";
-// import DaimondTine_App from "./AllTheme/DaimondTine/DaimondTine_App";
-// import Elveester_App from "./AllTheme/Elveester/Elveester_App";
+import DaimondTine_App from "./AllTheme/DaimondTine/DaimondTine_App";
+import Elveester_App from "./AllTheme/Elveester/Elveester_App";
 import { Storeinit } from "./utils/API/Home/Storeinit/Storeinit";
 import { CurrencyComboAPI } from "./utils/API/Combo/CurrencyComboAPI";
 import { MetalColorCombo } from "./utils/API/Combo/MetalColorCombo";
@@ -60,15 +60,15 @@ import { fetchPayMaster } from "./utils/API/OrderFlow/Paymaster";
 // const HemratnaProcatalog_App = React.lazy(() =>
 //   import("./AllTheme/hemratnaProcatalog/HemratnaProcatalog_App")
 // );
-// const Procatalog_App = React.lazy(() =>
-//   import("./AllTheme/Pocatalog/Procatalog_App")
-// );
+const Procatalog_App = React.lazy(() =>
+  import("./AllTheme/Pocatalog/Procatalog_App")
+);
 // const HouseOfQuadri_App = React.lazy(() =>
 //   import("./AllTheme/HouseOfQuadri/HouseOfQuadri_App")
 // );
-const ForEveryRoutes = React.lazy(() =>
-  import("./AllTheme/Forevery/ForeveryRoutes")
-);
+// const ForEveryRoutes = React.lazy(() =>
+//   import("./AllTheme/Forevery/ForeveryRoutes")
+// );
 // const Procatalog_MobileApp_App = React.lazy(() =>
 //   import("./AllTheme/MobileApp/Procatalog_MobileApp/Procatalog_MobileApp_App")
 // );
@@ -78,9 +78,9 @@ const ForEveryRoutes = React.lazy(() =>
 // const RoopJewellers_App = React.lazy(() =>
 //   import("./AllTheme/RoopJewellers/RoopJewellers_App")
 // );
-const MalakanJewels_App = React.lazy(() =>
-  import("./AllTheme/MalakanJwewls/MalakanJewels_App")
-);
+// const MalakanJewels_App = React.lazy(() =>
+//   import("./AllTheme/MalakanJwewls/MalakanJewels_App")
+// );
 
 export default function ThemeRoutes() {
   const smr_SetCompanyTitleLogo = useSetRecoilState(smr_companyLogo);
@@ -458,25 +458,25 @@ const Themes = ({ htmlContent }) => {
       <Suspense fallback={<></>}>
         {htmlContent?.rd[0]?.Themeno === 1 && <SmilingRock_App />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 2 && <DaimondTine_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 2 && <DaimondTine_App />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 3 && <Elveester_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 3 && <Elveester_App />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 4 && <SmilingRock_MobileApp_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 4 && <SmilingRock_MobileApp_App />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 5 && <HemratnaProcatalog_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 5 && <HemratnaProcatalog_App />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 6 && <Procatalog_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 6 && <Procatalog_App />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 7 && <HouseOfQuadri_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 7 && <HouseOfQuadri_App />}
 
         {htmlContent?.rd[0]?.Themeno === 8 && <ForEveryRoutes />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 9 && <Procatalog_MobileApp_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 9 && <Procatalog_MobileApp_App />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 10 && <StamFordJewels_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 10 && <StamFordJewels_App />}
 
-        {/* {htmlContent?.rd[0]?.Themeno === 11 && <RoopJewellers_App />} */}
+        {htmlContent?.rd[0]?.Themeno === 11 && <RoopJewellers_App />}
 
         {htmlContent?.rd[0]?.Themeno === 12 && <MalakanJewels_App />}
       </Suspense>
