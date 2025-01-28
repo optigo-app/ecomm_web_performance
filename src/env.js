6a997d29944a94bafa66657ef113ac0fdba28425
rajan <rajan@gmail.com>
2025-01-28 13:37:33 +0530

    all chnages done

--- src/env.js
+++ src/env.js
@@ -5,9 +5,9 @@
 // export const REACT_APP_WEB = "diamondtine.web";
 // export const REACT_APP_WEB = "hoq.web";
 // export const REACT_APP_WEB = "malakan.web";
-export const REACT_APP_WEB = "rpjewel.web";
+// export const REACT_APP_WEB = "rpjewel.web";
 // export const REACT_APP_WEB = "stamford.web";  
 // export const REACT_APP_WEB = "demostore";  
 // export const REACT_APP_WEB = "forevery.web";    // Forevery.web | Foreveryd.optigoapps.com  | world.forevery.one 
-// export const REACT_APP_WEB = "elvee.web";   // elvee.in
+export const REACT_APP_WEB = "elvee.web";   // elvee.in 
 // export const REACT_APP_WEB = "fgstorepro.mapp";   // procatalog mobile app
