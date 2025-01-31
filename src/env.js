c691afb961a622f2fe83e1a07648fa3603abe028
rajan <rajan@gmail.com>
2025-01-31 16:50:48 +0530

    forevry chnages

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-export const REACT_APP_WEB = "fgstore.web";
+// export const REACT_APP_WEB = "fgstore.web";
 // export const REACT_APP_WEB = "fgstore.pro";  // procatalog website
 // export const REACT_APP_WEB = "fgstore.plw";   //similing rock PLW
 // export const REACT_APP_WEB = "fgstore.mapp";   //similing rock mobile app
@@ -8,6 +8,6 @@ export const REACT_APP_WEB = "fgstore.web";
 // export const REACT_APP_WEB = "rpjewel.web"     ;
 // export const REACT_APP_WEB = "stamford.web";  
 // export const REACT_APP_WEB = "demostore";  
-// export const REACT_APP_WEB = "forevery.web";    // Forevery.web | Foreveryd.optigoapps.com  | world.forevery.one 
+export const REACT_APP_WEB = "forevery.web";    // Forevery.web | Foreveryd.optigoapps.com  | world.forevery.one 
 // export const REACT_APP_WEB = "elvee.web";   // elvee.in 
 // export const REACT_APP_WEB = "fgstorepro.mapp";   // procatalog mobile app
