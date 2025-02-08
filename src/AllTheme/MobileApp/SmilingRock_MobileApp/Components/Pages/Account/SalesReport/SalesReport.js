ce366f0ddb3563e860a47e9d701b3980c908cac5
rajan <rajan@gmail.com>
2025-02-08 13:58:12 +0530

    al done account rpice issue solved

--- src/AllTheme/MobileApp/SmilingRock_MobileApp/Components/Pages/Account/SalesReport/SalesReport.js
+++ src/AllTheme/MobileApp/SmilingRock_MobileApp/Components/Pages/Account/SalesReport/SalesReport.js
@@ -229,8 +229,21 @@ function EnhancedTableHead(props) {
   return (
     <TableHead className="salesReporttabelHead">
       <TableRow>
-        {headCells.map((headCell) => (
-          <TableCell
+        {headCells.map((headCell) => {
+              const headCellsLits = [
+                'MetalAmount',
+                'DiamondAmount',
+                'ColorStoneAmount',
+                'LabourAmount',
+                'OtherAmount',
+                'UnitCost'
+              ];
+              const {IsPriceShow} = JSON?.parse(sessionStorage?.getItem('storeInit')) ?? {} ;
+              if (IsPriceShow === 0 && headCellsLits.includes(headCell?.id)) {
+                return null;  
+              }
+                
+       return   <TableCell
             key={headCell.id}
             align={headCell.numeric ? "right" : "left"}
             padding={headCell.disablePadding ? "none" : "normal"}
@@ -257,7 +270,7 @@ function EnhancedTableHead(props) {
         </TableSortLabel>
          }
           </TableCell>
-        ))}
+})}
       </TableRow>
     </TableHead>
   );
@@ -661,6 +674,8 @@ const SalesReport = () => {
       tableContainer.scrollTop = 0;
     }
   };
+  const {IsPriceShow} = JSON?.parse(sessionStorage?.getItem('storeInit')) ?? {} ;
+
 
   return (
     <div className="salesReport_Account_SMRM">
@@ -668,7 +683,7 @@ const SalesReport = () => {
        <MobViewHeader title="Sales Report" />
     </div>
     <Box style={{marginBottom:'5rem'}}>
-      <Box
+     {IsPriceShow == 1 &&  <Box
         sx={{
           display: "flex",
           alignItems: "center",
@@ -778,7 +793,7 @@ const SalesReport = () => {
             )}
           </Box>
         </Box>
-      </Box>
+      </Box>}
       <Accordion sx={{marginBottom:'20px'}}>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           More FIlters
@@ -1023,7 +1038,8 @@ const SalesReport = () => {
                         <TableCell align="center">{row.SKUNo}</TableCell>
                         <TableCell align="center">{row.designno}</TableCell>
                         <TableCell align="center">{row.MetalType}</TableCell>
-                        <TableCell align="center">{row.MetalAmount}</TableCell>
+                     {IsPriceShow == 1 && <>
+                      <TableCell align="center">{row.MetalAmount}</TableCell>
                         <TableCell align="center">
                           {row.DiamondAmount}
                         </TableCell>
@@ -1033,6 +1049,7 @@ const SalesReport = () => {
                         <TableCell align="center">{row.LabourAmount}</TableCell>
                         <TableCell align="center">{row.OtherAmount}</TableCell>
                         <TableCell align="center">{row.UnitCost}</TableCell>
+                     </>}
                         <TableCell align="center">{row.Category}</TableCell>
                         <TableCell align="center">{row.GrossWt}</TableCell>
                         <TableCell align="center">{row.NetWt}</TableCell>
